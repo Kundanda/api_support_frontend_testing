2d59e4953fec68b4db15e47b6e7d58ddd32fd4c8
Kundan Dalmia <kundandalmia@gmail.com>
2019-03-23 19:18:16 +0800

    Updated code for sign in testing
    Updated code for sign in testing

--- SignInTesting.java
+++ SignInTesting.java
@@ -1,16 +1,11 @@
 package seleniumdemo;
 
-import java.sql.Time;
+
 
 
 import org.openqa.selenium.By;
-
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.Select;
-
-
 
 public class SignInTesting {
     
